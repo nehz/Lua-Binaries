9049494cb975cf171fe722f359840eb1a864c12a
Zhen Wang <mail@zhenwang.info>
2015-08-01 16:35:04 +0800

    Fixed build script

--- build/luajit.sh
+++ build/luajit.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 
-IXCODE=`xcode-select -print-path`
+IXCODE=/Applications/Xcode.app/Contents/Developer
 ISDK=$IXCODE/Platforms/iPhoneOS.platform/Developer
 ISDKVER=iPhoneOS8.4.sdk
 ISDKP=/usr/bin/
 ISDKF="-arch armv7 -isysroot $ISDK/SDKs/$ISDKVER"
-make HOST_CC="gcc -m32 -arch i386" CROSS=$ISDKP TARGET_FLAGS="$ISDKF" \
-     TARGET_SYS=iOS
 
 wget -O - http://luajit.org/download/LuaJIT-2.0.4.tar.gz | tar xz
 
-(cd LuaJIT-2.0.4 && \
-  make HOST_CC="gcc -m32 -arch i386" CROSS=$ISDKP TARGET_FLAGS="$ISDKF" \
+(cd "LuaJIT-2.0.4"; \
+  make HOST_CC="gcc -m32 -arch i386" CROSS="$ISDKP" TARGET_FLAGS="$ISDKF" \
   TARGET_SYS=iOS)
